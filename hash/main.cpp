e40c27dda236470f42003a469bafe82ec3ca75e9
Alex Filatov <tanyaarni@gmail.com>
2019-03-23 14:23:27 +0200

    Initial Commit

---
+++ hash/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hash
+//
+//  Created by Alex Filatov on 3/23/19.
+//  Copyright © 2019 Alex Filatov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
