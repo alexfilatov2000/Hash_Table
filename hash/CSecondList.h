2cc166ee48c1703fc9ebb072fbcc135b9d325e95
Alex Filatov <tanyaarni@gmail.com>
2019-03-25 21:14:16 +0200

    сделали поиск по ключю

--- hash/CSecondList.h
+++ hash/CSecondList.h
@@ -20,6 +20,19 @@ public:
         CPair<T1, T2> pair(key, value);
         pairList.push_front(pair);
     }
+
+    CList<CPair<T1, T2> > search(T1 _key){
+        CList<CPair<T1, T2> > list;
+        int n = pairList.getSize();
+        for (int i=0; i<n; i++){
+            CPair<T1, T2>& pair = pairList[i];
+            if (pair.key==_key) {
+                CPair<T1, T2> rez(_key, pair.value);
+                list.push_back(rez);
+            }
+        }
+        return list;
+    }
 };
 
 #endif // CSECONDLIST_H
